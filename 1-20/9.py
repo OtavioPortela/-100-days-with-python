14ff30e3e46da954703b44811998d200f21115d5
OtavioPortela <otavionegocios97@gmail.com>
2024-12-12 21:27:56 -0300

    day 9-2

--- 1-20/9.py
+++ 1-20/9.py
@@ -1,10 +1,19 @@
 def ordenar(lista):
-    pos = len(lista)
-    aux = lista[0]
-    for x in range(pos):
-        if lista[x] > aux:
-            aux = lista[x]
+    n = len(lista)
+    # Loop para controlar o número de passagens
+    for i in range(n):
+        troca = False
+        # Loop para comparar elementos adjacentes
+        for j in range(0, n - i - 1):
+            if lista[j] > lista[j + 1]:
+                # Trocar os elementos
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+                troca = True
+                # Se nenhuma troca aconteceu, a lista já está ordenada
+            if not troca:
+                break
+            return lista
 
-
-lista = [3,2,1]
-ordenar(lista)
+lista = [4,2,1]
+lista2 =ordenar(lista)
+print(lista2)
